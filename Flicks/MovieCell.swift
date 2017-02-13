6eae08e7552d3d00f30ba168d9c95a2aaf9b32e6
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-02-12 19:39:30 -0500

    Displays titles and descriptions, but not the poster images just yet

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Sanat Deshpande on 2/12/17.
+//  Copyright © 2017 Sanat Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
